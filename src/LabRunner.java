4a863e8bfc4390aaa34a27fe0d8e656a18f188ad
BT_3W2_16 <chloed@nycstudents.net>
2022-10-11 09:27:37 -0400

    new print statements

---
+++ src/LabRunner.java
@@ -0,0 +1,17 @@
+public class LabRunner {
+    public static void main(String[] args) {
+
+        String greeting = "Hello, friend!\n";
+        System.out.print(greeting);
+        System.out.print("Nice to see you!\n");
+        System.out.println("Goodbye!");
+
+
+
+
+
+
+
+
+    }
+}
